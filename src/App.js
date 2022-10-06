7c31965fe4f353924c99fe893a237c5b52995cf8
Kapadokia <ktnyandoch@gmail.com>
2022-10-06 16:20:40 +0300

    Updated Css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import NavBar from './components/nav/nav';
-import './theme.min.css'
 import Home from './components/home';
 import Products from './components/products/products';
 import Categories from './components/products/categories';
